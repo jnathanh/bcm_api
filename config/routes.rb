932f25613471daae96bced7bd9414bf183ea8779
Nathan Hanna <jnathanh@gmail.com>
2018-05-28 12:46:30 -0600

    scaffolded MeasurementType model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :measurement_types
   resources :checkpoints
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
